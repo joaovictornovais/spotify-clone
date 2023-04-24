e8d988a0ce0c96b3127a106f91c5e33b68be0858
João Victor <joao.dipaay@gmail.com>
2023-04-24 15:03:32 -0300

    Utilizando objetos

--- src/components/MadeFor.js
+++ src/components/MadeFor.js
@@ -1,36 +1,59 @@
 import React from 'react'
 
 const MadeFor = () => {
+
+  const playlists = [
+    {
+      id: 1,
+      name: "Descobertas da Semana",
+      description: "Sua mixtape semanal cheia de novas descobertas e pérolas musicais escolhidas só pra você. Atualiza toda segunda.",
+      image: "/album7.jpg",
+      favorites: false,
+    },
+    {
+      id: 2,
+      name: "Daily Mix 1",
+      description: "Metro Boomin, XXXTENTACION, Future e mais",
+      image: "/album8.jpg",
+      favorites: false,
+    },
+    {
+      id: 3,
+      name: "Daily Mix 2",
+      description: "Gunna, Lil Uzi Vert, Playboi Carti e mais",
+      image: "/album9.jpg",
+      favorites: false,
+    },
+    {
+      id: 4,
+      name: "Daily Mix 3",
+      description: "Yunk Vino, Derek, Veigh e mais",
+      image: "/album10.jpg",
+      favorites: false,
+    },
+    {
+      id: 5,
+      name: "Daily Mix 4",
+      description: "Kordhell, GRAVECHILL, PRXSXNT FXTURE e mais",
+      image: "/album11.jpg",
+      favorites: false,
+    },
+  ]
+
   return (
     <>
     <h2 className="text-2xl font-bold mt-10">Feito para João Victor</h2>
-          <div className="grid grid-cols-8 gap-4 mt-4">
-            <a href="#" className="bg-white/5 p-3 rounded-md hover:bg-white/10 flex flex-col gap-2">
-              <img src="/album7.jpg" className="w-full rounded" alt="Descobertas da Semana" width={10} height={10}/>
-              <strong className="max-w-[150px] whitespace-nowrap overflow-hidden text-ellipsis font-semibold">Descobertas da Semana</strong>
-              <span className="overflow-hidden text-ellipsis line-clamp-2 text-zinc-500">Sua mixtape semanal cheia de novas descobertas e pérolas musicais escolhidas só pra você. Atualiza toda segunda.</span>
-            </a>
-            <a href="#" className="bg-white/5 p-3 rounded-md hover:bg-white/10 flex flex-col gap-2">
-              <img src="/album8.jpg" className="w-full rounded" alt="Daily Mix 1" width={10} height={10}/>
-              <strong className="max-w-[150px] whitespace-nowrap overflow-hidden text-ellipsis font-semibold">Daily Mix 1</strong>
-              <span className="overflow-hidden text-ellipsis line-clamp-2 text-zinc-500">Metro Boomin, XXXTENTACION, Future e mais</span>
-            </a>
-            <a href="#" className="bg-white/5 p-3 rounded-md hover:bg-white/10 flex flex-col gap-2">
-              <img src="/album9.jpg" className="w-full rounded" alt="Daily Mix 2" width={10} height={10}/>
-              <strong className="max-w-[150px] whitespace-nowrap overflow-hidden text-ellipsis font-semibold">Daily Mix 2</strong>
-              <span className="overflow-hidden text-ellipsis line-clamp-2 text-zinc-500">Gunna, Lil Uzi Vert, Playboi Carti e mais</span>
-            </a>
-            <a href="#" className="bg-white/5 p-3 rounded-md hover:bg-white/10 flex flex-col gap-2">
-              <img src="/album10.jpg" className="w-full rounded" alt="Daily Mix 3" width={10} height={10}/>
-              <strong className="max-w-[150px] whitespace-nowrap overflow-hidden text-ellipsis font-semibold">Daily Mix 3</strong>
-              <span className="overflow-hidden text-ellipsis line-clamp-2 text-zinc-500">Yunk Vino, Derek, Veigh e mais</span>
-            </a>
-            <a href="#" className="bg-white/5 p-3 rounded-md hover:bg-white/10 flex flex-col gap-2">
-              <img src="/album11.jpg" className="w-full rounded" alt="Daily Mix 4" width={10} height={10}/>
-              <strong className="max-w-[150px] whitespace-nowrap overflow-hidden text-ellipsis font-semibold">Daily Mix 4</strong>
-              <span className="overflow-hidden text-ellipsis line-clamp-2 text-zinc-500">Kordhell, GRAVECHILL, PREXSXNT FXTURE e mais</span>
-            </a>
-          </div>
+      <div className="grid grid-cols-8 gap-4 mt-4">
+        {
+        playlists.map((playlist) => (
+          <a href="#" className="bg-white/5 p-3 rounded-md hover:bg-white/10 flex flex-col gap-2">
+            <img src={playlist.image} className="w-full rounded" alt={`Playlist ${playlist.name}`} width={10} height={10}/>
+            <strong className="max-w-[150px] whitespace-nowrap overflow-hidden text-ellipsis font-semibold">{playlist.name}</strong>
+            <span className="overflow-hidden text-ellipsis line-clamp-2 text-zinc-500">{playlist.description}</span>
+          </a>
+        ))
+        }
+      </div>
     </>
   )
 }
--- src/components/Recommended.js
+++ src/components/Recommended.js
@@ -2,52 +2,66 @@ import { Play } from 'lucide-react'
 import React from 'react'
 
 const Recommended = () => {
+
+  const albums = [
+    {
+      id: 1,
+      name: "Blonde",
+      artist: "Frank Ocean",
+      image: "/album1.jpg",
+      favorites: true,
+    },
+    {
+      id: 2,
+      name: "Savage Mode II",
+      artist: "21Savage, Metro Boomin",
+      image: "/album2.jpg",
+      favorites: true,
+    },
+    {
+      id: 3,
+      name: "Donda",
+      artist: "Kanye West",
+      image: "/album3.jpeg",
+      favorites: true,
+    },
+    {
+      id: 4,
+      name: "Whole Lotta Red",
+      artist: "Playboi Carti",
+      image: "/album4.png",
+      favorites: true,
+    },
+    {
+      id: 5,
+      name: "Certified Lover Boy",
+      artist: "Drake",
+      image: "/album5.jpg",
+      favorites: true,
+    },
+    {
+      id: 6,
+      name: "Dark Lane Demo Tapes",
+      artist: "Drake",
+      image: "/album6.jpg",
+      favorites: true,
+    }
+  ]
   return (
     <>
     <h1 className="text-3xl font-bold mt-10">Boa tarde</h1>
     <div className="grid grid-cols-3 gap-6 mt-5">
-      <a href="#" className="bg-white/5 rounded group flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
-        <img src="/album1.jpg" alt="Capa do Álbum Blonde do rapper Frank Ocean " width={70} height={70}/>
-        <strong>Blonde</strong>
-        <button className="w-8 h-8 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-3 invisible group-hover:visible ">
-          <Play size={20}/>
-        </button>
-      </a>
-      <a href="#" className="bg-white/5 rounded group flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
-        <img src="/album2.jpg" alt="Capa do Álbum Savage Mode II do rapper 21Savage " width={70} height={70}/>
-        <strong>Savage Mode II</strong>
-        <button className="w-8 h-8 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-3 invisible group-hover:visible ">
-          <Play size={20}/>
-        </button>
-      </a>
-      <a href="#" className="bg-white/5 rounded group flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
-        <img src="/album3.jpeg" alt="Capa do Álbum Donda do rapper Kanye West" width={70} height={70}/>
-        <strong>Donda</strong>
-        <button className="w-8 h-8 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-3 invisible group-hover:visible ">
-          <Play size={20}/>
-        </button>
-      </a>
-      <a href="#" className="bg-white/5 rounded group flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
-        <img src="/album4.png" alt="Capa do Álbum Whole Lotta Red do rapper Playboi Carti " width={70} height={70}/>
-        <strong>Whole Lotta Red</strong>
-        <button className="w-8 h-8 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-3 invisible group-hover:visible ">
-          <Play size={20}/>
-        </button>
-      </a>
-      <a href="#" className="bg-white/5 rounded group flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
-        <img src="/album5.jpg" alt="Capa do Álbum Certified Lover Boy do rapper Drake " width={70} height={70}/>
-        <strong>Certified Lover Boy</strong>
-        <button className="w-8 h-8 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-3 invisible group-hover:visible ">
-          <Play size={20}/>
-        </button>
-      </a>
-      <a href="#" className="bg-white/5 rounded flex group items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
-        <img src="/album6.jpg" alt="Capa do Álbum Dark Lane Demo Tapes do rapper Drake" width={70} height={70}/>
-        <strong>Dark Lane Demo Tapes</strong>
-        <button className="w-8 h-8 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-3 invisible group-hover:visible ">
-          <Play size={20}/>
-        </button>
-      </a>
+      {
+        albums.map((album) => (
+          <a href="#" className="bg-white/5 rounded group flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
+            <img src={album.image} alt={`Capa do álbum ${album.name}, de ${album.artist}`} width={70} height={70}/>
+            <strong>{album.name}</strong>
+            <button className="w-8 h-8 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-3 invisible group-hover:visible ">
+              <Play size={20}/>
+            </button>
+          </a>
+        ))
+      }
     </div>
     </>
   )
