0e05f362e60dd346555d271aa90ce23525c54db6
João Victor <joao.dipaay@gmail.com>
2023-04-25 10:28:14 -0300

    fix: main height

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="bg-zinc-900 h-screen text-zinc-50 flex flex-col overflow-hidden">
       <div className="flex flex-1">
         <Siderbar />
-        <main className="flex-1 ml-[288px] p-6 overflow-y-auto h-[50%] 
+        <main className="flex-1 ml-[288px] p-6 overflow-y-auto h-[calc(100vh-119px)] 
         scrollbar-thumb-gray-300
         scrollbar-thin
         scrollbar-h-2">
