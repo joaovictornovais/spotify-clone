2a263c0fb01d307172354dd2bf7bbc591ee74e67
João Victor <joao.dipaay@gmail.com>
2023-04-26 10:26:27 -0300

    Responsive up nav

--- src/components/UpNav.js
+++ src/components/UpNav.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const UpNav = () => {
   return (
-    <div className="flex justify-between">
+    <div className="flex justify-between gap-3">
       <div className="flex items-center gap-3">
         <button className="bg-black/20 rounded-full">
           <ChevronLeft/>
@@ -13,10 +13,10 @@ const UpNav = () => {
         </button>
       </div>
       <div className="flex gap-3">
-        <button className="font-bold px-5 py-2 border-white border-2 rounded-3xl">Faça upgrade</button>
+        <button className="font-bold px-3 py-1 md:px-5 md:py-2 border-white border-2 rounded-3xl text-sm md:text-lg">Faça upgrade</button>
         <button className="flex items-center gap-3 bg-black/80 p-1 rounded-full">
           <img src="/perfil.jpg" width={40} className="rounded-full" />
-          <strong>João Victor</strong>
+          <strong className="whitespace-nowrap overflow-hidden text-ellipsis">João Victor</strong>
           <ChevronDown className="mr-1"/>
         </button>
       </div>
